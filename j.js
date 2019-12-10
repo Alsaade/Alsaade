f02442b2565554b90256aac7472ab939a4bb27ba
Alsaade <55971525+Alsaade@users.noreply.github.com>
2019-12-10 14:05:48 +0200

    Add files via upload

---
+++ j.js
@@ -0,0 +1,5 @@
+const Locations = document.getElementById('Locations');
+const Programs = document.getElementById('Programs');
+const About = document.getElementById('About');
+const Home = document.getElementById('Home');
+
